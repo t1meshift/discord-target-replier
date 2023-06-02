e09aeb91225b1ef46e4d30214b424ffef904f7f1
Yury Kurlykov <sh1ftr@protonmail.ch>
2023-06-02 12:49:34 +0400

    Config: Add custom fields support

--- Config.h
+++ Config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <optional>
 #include <string>
 #include <vector>
 #include <nlohmann/json.hpp>
@@ -20,6 +21,26 @@ namespace griha {
 		[[nodiscard]] std::string reaction() const;
 		[[nodiscard]] uint8_t response_probability() const;
 		[[nodiscard]] std::vector<std::string> replies() const;
+
+		template<typename T>
+		[[nodiscard]] std::optional<T> field(const std::string& field_name) const {
+			if (!config_.contains(field_name)) {
+				return std::nullopt;
+			}
+			return get<T>(field_name);
+		}
+
+		template<typename T>
+		[[nodiscard]] std::optional<T> field(std::initializer_list<std::string> field_path) const {
+			nlohmann::json obj = config_;
+			for (const auto& path : field_path) {
+				if (!obj.contains(path)) {
+					return std::nullopt;
+				}
+				obj = obj.at(path);
+			}
+			return obj.get<T>();
+		}
 	private:
 		nlohmann::json config_;
 
